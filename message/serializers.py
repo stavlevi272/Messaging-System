48b0c48b4d19a5e4d889df0e38a85b1622060616
stivy <stavlevi@campus.technion.ac.il>
2021-08-17 20:03:13 +0300

    add message id to message serializer

--- message/serializers.py
+++ message/serializers.py
@@ -4,16 +4,17 @@ from .models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
     def serialize_to_json(messages):
-        message_num = 1
+        index = 1
         user_all_messages = {}
         for message in messages:
             form = {
+                'message_id' : message.id,
                 'sender': message.sender.username,
                 'receiver': message.receiver.username,
                 'subject': message.subject,
                 'content': message.content,
                 'creation date': message.creation_date,
             }
-            user_all_messages[message_num] = form
-            message_num += 1
+            user_all_messages[index] = form
+            index += 1
         return user_all_messages
